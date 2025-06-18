48a0ff7a35108f69eab96e0433a6385515a7970a
yhfajar <yurliansyah.hirma@bssn.go.id>
2025-06-18 08:23:42 +0700

    feat:add users schema into db

--- db/db.go
+++ db/db.go
@@ -24,6 +24,20 @@ func InitDB() {
 
 }
 func createTabel() {
+	createUserTabel := `
+	CREATE TABLE IF NOT EXISTS users(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	)
+	`
+
+	_, err := DB.Exec(createUserTabel)
+
+	if err != nil {
+		panic("failed create table users!")
+	}
+
 	createEventTable := `
 	CREATE TABLE IF NOT EXISTS events(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,13 +45,14 @@ func createTabel() {
 		description TEXT NOT NULL,
 		location TEXT NOT NULL,
 		dateTime DATETIME NOT NULL,
-		user_id INTEGER
+		user_id INTEGER,
+		FOREIGN KEY(user_id) REFERENCES users(id)
 	)
 	`
 
-	_, err := DB.Exec(createEventTable)
+	_, err = DB.Exec(createEventTable)
 
 	if err != nil {
-		panic("failed create table events!a")
+		panic("failed create table events!")
 	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -3,9 +3,14 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
+
+	//Events Routes
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 	server.PUT("/events/:id", updateEvent)
 	server.DELETE("/events/:id", deleteEvent)
+
+	//Users Routes
+	server.POST("/signup")
 }
